22f2c4ce66cabcfe76e9dac78fda66eaf194d5c8
Gomes316 <ivangomes316@gmail.com>
2025-02-24 11:15:27 +0000

    Criei as definições pra a primeira carta do super

--- CartasSupertrunfo.c
+++ CartasSupertrunfo.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
-//Definições das informações da primeira
+//Definições das informações da primeira carta
 int main(){
+    int populacao1 = 210000000;
+    char estado1 [] = 'A';
+    char nome1  = "Salvador";
+    float area1 = 696.8;
+    float pib1 = 62.9;
+    int pontos1 = 20;
+    char codigo1  = "A01";
 
-
-
-
-
-
+//Difinições das informações da segunda carta
 
 
 
